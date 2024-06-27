98ebe05a30bc766dbbd1e5d7855ffa886271ec75
Jumael Ahmed Khan <jumael99@gmail.com>
2024-06-27 13:04:46 +0600

    db.js file issue resolved

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,8 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import * as console from "node:console";
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Load environment variables from .env file in the root directory
-//Since my .env file is in root directory
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
