34fd5c9ca09a273a61376dd773281f85e3a64105
Jumael Ahmed Khan <jumael99@gmail.com>
2024-06-27 13:05:46 +0600

    followed the best practices for profileController file

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,38 +1,35 @@
 import Profile from '../models/profileModel.js';
+import asyncHandler from "../middleware/asyncHandler.js";
 
-// GET Profile Controller
-export const getProfile = async (req, res) => {
-    try {
-        // Assuming req.user contains the logged-in user's data
-        const profile = await Profile.findOne({ email: "john.doe@example.com" });
-        if (!profile) {
-            return res.status(404).json({ message: 'Profile not found' });
-        }
-        res.json(profile);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+// @desc    Get user profile
+// @route   GET /profile
+// @access  Public
+export const getProfile = asyncHandler(async (req, res) => {
+    // Assuming req.user contains the logged-in user's data
+    const profile = await Profile.findOne({ email: "imtiaj.hossain@example.com" });
+    if (!profile) {
+        return res.status(404).json({ message: 'Profile not found' });
     }
-};
+    res.json(profile);
+});
 
-// UPDATE Profile Controller
-export const updateProfile = async (req, res) => {
-    try {
-        const updatedData = req.body;
+// @desc    Edit user profile
+// @route   PUT /profile
+// @access  Public
+export const updateProfile = asyncHandler(async (req, res) => {
+    const updatedData = req.body;
 
-        // Find the Profile by the logged-in user's email and update it
-        // Assuming req.user contains the logged-in user's data
-        const profile = await Profile.findOneAndUpdate(
-            { email: "john.doe@example.com" },
-            updatedData,
-            { new: true, runValidators: true }
-        );
+    // Find the Profile by the logged-in user's email and update it
+    // Assuming req.user contains the logged-in user's data
+    const profile = await Profile.findOneAndUpdate(
+        { email: "john.doe@example.com" },
+        updatedData,
+        { new: true, runValidators: true }
+    );
 
-        if (!profile) {
-            return res.status(404).json({ message: 'Profile not found' });
-        }
-
-        res.json(profile);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+    if (!profile) {
+        return res.status(404).json({ message: 'Profile not found' });
     }
-};
+
+    res.json(profile);
+});
