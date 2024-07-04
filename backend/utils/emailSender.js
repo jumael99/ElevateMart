bd51a11ca3ef5ee64d0d57d935d4824809e09add
Sp3cTer <20103033@iubat.edu>
2024-07-04 15:44:01 +0600

    Fixed a small bug related to email

--- backend/utils/emailSender.js
+++ backend/utils/emailSender.js
@@ -5,27 +5,14 @@ import {
 } from "./emailTemplate.js";
 
 // Transporter configuration gmail
-// const createTransporter = () => {
-//   const transport = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 587,
-//     secure: false,
-//     auth: {
-//       user: process.env.GMAIL_USER,
-//       pass: process.env.GMAIL_PASS,
-//     },
-//   });
-//   return transport;
-// };
-
-// Transporter configuration mailtrap
 const createTransporter = () => {
   const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
     },
   });
   return transport;
