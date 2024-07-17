51b96aca0b31acfd943c306a4447f01f97afb3a2
Rizraizadda <rizraizada000@gmail.com>
2024-07-18 02:31:01 +0600

    tt

--- frontend/store/slices/api/subcategoryApiSlice.js
+++ frontend/store/slices/api/subcategoryApiSlice.js
@@ -1,19 +1,21 @@
 import apiSlice from "./apiSlice";
-import { subcatagory_URL } from "./constantURL";  
+import { subcatagory_URL } from "./constantURL";
 
 export const subcatagoriesApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     fetchAllSubCategories: builder.query({
       query: () => ({
-        url: subcatagory_URL,  
+        url: subcatagory_URL,
         method: "GET",
       }),
+      providesTags: ["SubCategory"],
     }),
     fetchSubCategoryBySlug: builder.query({
       query: (slug) => ({
         url: `${subcatagory_URL}/${slug}`,
         method: "GET",
       }),
+      providesTags: (result, error, slug) => [{ type: "SubCategory", id: slug }],
     }),
     createNewSubCategory: builder.mutation({
       query: (subCategory) => ({
@@ -21,6 +23,7 @@ export const subcatagoriesApi = apiSlice.injectEndpoints({
         method: "POST",
         body: subCategory,
       }),
+      invalidatesTags: ["SubCategory"],
     }),
     updateSubCategory: builder.mutation({
       query: ({ id, ...subCategory }) => ({
@@ -28,12 +31,14 @@ export const subcatagoriesApi = apiSlice.injectEndpoints({
         method: "PATCH",
         body: subCategory,
       }),
+      invalidatesTags: ["SubCategory"],
     }),
     deleteSubCategory: builder.mutation({
       query: (id) => ({
         url: `${subcatagory_URL}/${id}`,
         method: "DELETE",
       }),
+      invalidatesTags: ["SubCategory"],
     }),
   }),
 });
